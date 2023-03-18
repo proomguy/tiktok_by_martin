fdcaf8c447daed7b4eba8f8fcabdb20fc04a077c
proomguy <mattz100bc@gmail.com>
2023-03-18 16:17:12 +0300

    Created Email and password input_text_widget.dart

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tiktok_remake/widgets/input_text_widget.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,10 +10,68 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 100,),
+              Image.asset(
+                "images/tiktok.png",
+                width: 200,
+              ),
+              Text(
+                "Welcome to TikTok",
+                style: GoogleFonts.acme(
+                  fontSize: 34,
+                  color: Colors.cyan,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Glad to see you",
+                style: GoogleFonts.abhayaLibre(
+                  fontSize: 34,
+                  color: Colors.cyan.shade300,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              const SizedBox(height: 30,),
+
+              //Email Input Field
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: InputTextWidget(
+                  textEditingController: emailTextEditingController,
+                  labelString: "Email",
+                  isObscure: false,
+                  iconData: Icons.email_outlined,
+                ),
+              ),
+              const SizedBox(height: 25,),
+              //Password Input Field
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: InputTextWidget(
+                  textEditingController: passwordTextEditingController,
+                  labelString: "Password",
+                  isObscure: true,
+                  iconData: Icons.lock_outline,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/input_text_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class InputTextWidget extends StatelessWidget {
+
+  final TextEditingController textEditingController;
+  final IconData? iconData;
+  final String? assetReference;
+  final String labelString;
+  final bool isObscure;
+
+  InputTextWidget({
+    required this.textEditingController,
+    this.iconData,
+    this.assetReference,
+    required this.labelString,
+    required this.isObscure,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: textEditingController,
+      decoration: InputDecoration(
+        labelText: labelString,
+        prefixIcon: iconData != null
+            ? Icon(iconData)
+            : Padding(
+          padding: const EdgeInsets.all(8),
+          child: Image.asset(assetReference!, width: 10,),
+        ),
+        labelStyle: const TextStyle(
+          fontSize: 18,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(
+            color: Colors.grey
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(
+              color: Colors.greenAccent
+          ),
+        ),
+      ),
+      obscureText: isObscure,
+
+    );
+  }
+}
