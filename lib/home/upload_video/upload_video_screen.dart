04fe77308e156f99b081c3432d7c72600e7cb72a
proomguy <mattz100bc@gmail.com>
2023-04-06 04:23:51 +0300

    Designing upload video screen

--- lib/home/upload_video/upload_video_screen.dart
+++ lib/home/upload_video/upload_video_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class UploadVideoScreen extends StatefulWidget {
   const UploadVideoScreen({Key? key}) : super(key: key);
@@ -8,6 +9,99 @@ class UploadVideoScreen extends StatefulWidget {
 }
 
 class _UploadVideoScreenState extends State<UploadVideoScreen> {
+
+  displayDialogBox(){
+    return showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+          backgroundColor: Colors.blueGrey,
+          children: [
+            SimpleDialogOption(
+              onPressed: (){
+
+              },
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.image,
+                    size: 24,
+                    color: Colors.green,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        'Get from Gallery',
+                        maxLines: 2,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.green
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SimpleDialogOption(
+              onPressed: (){
+
+              },
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.camera_enhance_outlined,
+                    size: 24,
+                    color: Colors.orange,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        'Capture with Phone Camera',
+                        maxLines: 2,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SimpleDialogOption(
+              onPressed: (){
+                Get.back();
+
+              },
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.cancel_outlined,
+                    size: 24,
+                    color: Colors.red,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +117,7 @@ class _UploadVideoScreenState extends State<UploadVideoScreen> {
             ElevatedButton(
               onPressed: (){
                 //Open a dialog Box for user to choose source of video
-
+                displayDialogBox();
                 },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.green,
